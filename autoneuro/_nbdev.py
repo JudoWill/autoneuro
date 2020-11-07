b5fe12aa07a539dfe39d29e85a329d34b10a7d75
Will <judowill@gmail.com>
2020-11-07 15:34:08 -0500

    Rearranging file structure

--- autoneuro/_nbdev.py
+++ autoneuro/_nbdev.py
@@ -2,21 +2,27 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"LookupNormalTransform": "00_core.ipynb",
-         "FILTER_MAPPINGS": "00_core.ipynb",
-         "build_merged_transformers": "00_core.ipynb",
-         "build_from_yaml": "00_core.ipynb",
-         "EncodingTransform": "00_core.ipynb",
-         "ScaledRegressionNode": "00_core.ipynb",
-         "ScaledRegressionTransform": "00_core.ipynb",
-         "LoadRawAndNormMixin": "01_domains.ipynb",
-         "AbstractTest": "01_domains.ipynb",
-         "AbstractDomain": "01_domains.ipynb",
-         "MemoryDomain": "01_domains.ipynb"}
+index = {"AbstractCalculator": "00_calculators.ipynb",
+         "AbstractOperation": "00_calculators.ipynb",
+         "EquationOperation": "00_calculators.ipynb",
+         "NormativeLookup": "00_calculators.ipynb",
+         "BinnedScalingOperator": "00_calculators.ipynb",
+         "RegressionNormOperator": "00_calculators.ipynb",
+         "LoadRawAndNormMixin": "03_domains.ipynb",
+         "AbstractTest": "03_domains.ipynb",
+         "AbstractDomain": "03_domains.ipynb",
+         "MemoryDomain": "03_domains.ipynb",
+         "LookupNormalTransform": "09_core.ipynb",
+         "FILTER_MAPPINGS": "09_core.ipynb",
+         "build_merged_transformers": "09_core.ipynb",
+         "build_from_yaml": "09_core.ipynb",
+         "EncodingTransform": "09_core.ipynb",
+         "ScaledRegressionNode": "09_core.ipynb",
+         "ScaledRegressionTransform": "09_core.ipynb"}
 
-modules = ["core.py",
+modules = ["calculators.py",
            "domains.py",
-           "calculators.py"]
+           "core.py"]
 
 doc_url = "https://judowill.github.io/autoneuro/"
 
